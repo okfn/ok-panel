9c3c3e5a42baa81cbf50a7066c8b5b084b8d3bd1
Sam Smith <sam@mintcanary.com>
2015-05-25 15:11:46 +0100

    WP plugin files

---
+++ assets/js/frontend.js
@@ -0,0 +1,8 @@
+jQuery( document ).ready( function ( e ) {
+  $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
+  
+  $('a.ok-ribbon').click(function(e) {
+      $(this).toggleClass("closed open");
+      $('body > #ok-panel').toggleClass("closed open");
+  });
+});

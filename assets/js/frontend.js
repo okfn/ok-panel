81097411dd98aa69cd9a944c7851b73272270ee6
Sam Smith <sam@mintcanary.com>
2015-05-26 10:38:40 +0100

    min width

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,8 +1,10 @@
 jQuery( document ).ready( function ( e ) {
-  $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
-  
-  $('a.ok-ribbon').click(function(e) {
-      $(this).toggleClass("closed open");
-      $('body > #ok-panel').toggleClass("closed open");
-  });
+  if ( $(document).width() > 560) { 
+    $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
+    
+    $('a.ok-ribbon').click(function(e) {
+        $(this).toggleClass("closed open");
+        $('body > #ok-panel').toggleClass("closed open");
+    });
+  }
 });
